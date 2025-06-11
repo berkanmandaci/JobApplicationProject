bfef648ae67b8b62f280e890d3d3f207ca452f3a
Berkan <berkan_mandaci@hotmail.com>
2025-06-11 03:25:35 +0300

    Enhance Job Application Project with Docker support and database integration. Added docker-compose.yml for service orchestration, updated .gitignore to include environment files, and implemented Entity Framework with PostgreSQL for task management. Created TaskItemsController for CRUD operations and added necessary migrations.

--- JobApplicationProject/Program.cs
+++ JobApplicationProject/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using JobApplicationProject.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +25,13 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<TodoContext>();
+    dbContext.Database.Migrate();
+}
 
 app.MapControllers();
 
